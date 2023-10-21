6e37c2149dc91e292d37eedabb02a6c487f03a60
Krishna Keshava Das <daskrishna0902@gmail.com>
2023-10-21 19:49:50 +0530

    files changed

---
+++ client/src/components/Login.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import './login.css'
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post('http://localhost:5000/login', { username: username, password: password }).then(navigate('/main'))
+      const { token } = response.data;
+      console.log(token);
+    } catch (error) {
+      console.error(error); 
+    }
+  };
+
+  return (
+    <div className='login-container'>
+      <h2 className='title'>Login Here</h2>
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        className='input-field'
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        className='input-field'
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleLogin} className='login-button'>Login</button>
+    </div>
+  );
+};
+
+export default Login;
---
+++ client/src/components/MainPage.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import { QRCodeSVG } from 'qrcode.react';
+import Axios from 'axios';
+import './main.css'
+
+const MainPage = () => {
+  const [qrValue, setQrValue] = useState('');
+  const [generatedQRCode, setGeneratedQRCode] = useState(null);
+
+  const handleInputChange = (e) => {
+    setQrValue(e.target.value);
+  };
+
+  const generateQRCode = async () => {
+    if (qrValue.trim() !== '') {
+      setGeneratedQRCode(qrValue);
+
+      // Send the QR code value to the backend and save it
+      await Axios.post('http://localhost:5000/qrcodes', { content: qrValue })
+        .then((response) => {
+          console.log(response.data);
+        })
+        .catch((error) => {
+          console.error('Error saving QR code:', error);
+        });
+      
+        setQrValue('');
+    } else {
+      alert('Please enter a valid QR code value.');
+    }
+  };
+
+  return (
+    <div className='main-div'>
+      <header className='container'>
+        <p className='title'>Scan my QR</p>
+        <input
+          type="text"
+          placeholder="Enter QR code value"
+          value={qrValue}
+          onChange={handleInputChange}
+          className='input-box'
+        />
+        {generatedQRCode && <QRCodeSVG value={generatedQRCode} size={256} className='qr-code-img'/>}
+        <button onClick={generateQRCode}>Generate QR Code</button>
+        
+      </header>
+    </div>
+  );
+};
+
+export default MainPage;
